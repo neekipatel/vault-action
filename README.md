562762b06815acb00eefdb35d0e5f2d260c58b50
Neeki Patel <neekipatel@gmail.com>
2019-11-16 19:32:39 -0500

    init test

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # vault-action
 Add Hashicorp Vault's secrets to environment variables
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+uses: actions/hello-world-javascript-action@v1
+with:
+  who-to-greet: 'Mona the Octocat'
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const VaultClient = require('node-vault');
+
+(async () => {
+  try {
+    const endpoint = core.getInput('endpoint', { required: true });
+    const token = core.getInput('token', { required: true });
+
+
+    const vault = VaultClient({
+      apiVersion: 'v1',
+      endpoint: endpoint,
+      token: token
+    });
+
+
+    const kv = core.getInput('kv');
+
+    console.log("kv = ", kv);
+
+    const value = await vault.read('secret/hello')
+
+    core.exportVariable('hello', 'value');
+    core.setOutput('hi', 'their');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
